b6872a7fc93ec5b625c95577a08956e07153e62b
tarikozturk017 <tarik.ozturkk1@gmail.com>
2023-02-10 21:43:22 -0500

    list projects

--- portfolio/pages/components/Projects.js
+++ portfolio/pages/components/Projects.js
@@ -1,10 +1,14 @@
+import Image from "next/image";
+import img from "../../public/auctionhub.jpg"
+
+
 const Projects = () => {
   
   return (
         <div>
           <div className=" flex justify-around">
           <h3 className="text-xl font-medium  dark:text-gray-200">List of My Projects</h3>
-          <h3 className="text-xl font-medium  dark:text-gray-200">Project Details</h3>
+          {/* <h3 className="text-xl font-medium  dark:text-gray-200">Project Details</h3> */}
           </div>
         <div className="w-auto lg:flex gap-10">
             
@@ -30,7 +34,11 @@ const Projects = () => {
             </div>
 
             <div className="text-center shadow-xl shadow-gray-200 dark:shadow-gray-700 p-10 rounded-xl my-10   flex-1">
-            <img className=" object-center m-auto" width={"300px"} src="https://az837918.vo.msecnd.net/publishedimages/articles/1733/en-CA/images/cover/1/free-download-this-stunning-alberta-scene-for-your-device-background-image-L-6.jpg" alt="" />
+            {/* <img className=" object-center m-auto" width={"300px"} src="https://az837918.vo.msecnd.net/publishedimages/articles/1733/en-CA/images/cover/1/free-download-this-stunning-alberta-scene-for-your-device-background-image-L-6.jpg" alt="" /> */}
+              <div className=" block">
+                <Image className=" mx-auto max-w-2xl h-auto transition-shadow ease-in-out duration-300 shadow-none hover:shadow-xl rounded" width={"100%"}  height={"100%"} layout={"responsive"} src={img} />
+              </div>
+
               <h3 className="text-lg font-medium pt-8 pb-2 dark:text-gray-200">
                 Reasype
               </h3>
