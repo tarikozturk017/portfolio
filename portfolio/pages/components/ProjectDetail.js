6de7b97b6426dad7cb9138711bcbea4942135eef
tarikozturk017 <tarik.ozturkk1@gmail.com>
2023-02-12 20:28:55 -0500

    fix

--- portfolio/pages/components/ProjectDetail.js
+++ portfolio/pages/components/ProjectDetail.js
@@ -4,10 +4,10 @@ import HoverBox from "./HoverBox"
 const ProjectDetail = (props) => {
     const {
         // name,
-        description,
-        technologies,
-        images,
-        url
+        // description,
+        // technologies,
+        // images,
+        // url
     } = props.project
 
     return (
@@ -16,11 +16,11 @@ const ProjectDetail = (props) => {
             <div className=" block">
             {/* <Image className=" mx-auto max-w-2xl h-auto transition-shadow ease-in-out duration-300 shadow-none hover:shadow-xl rounded" 
                 width={"100%"}  height={"100%"} alt='' src={images.static} /> */}
-                <HoverBox static={images.static} playGif={images.dynamic} />
+                <HoverBox static={props.project.images.static} playGif={props.project.images.dynamic} />
             </div>
-            {technologies[1] ? 
+            {props.project.technologies[1] ? 
             <ul className=" justify-between flex px-16 my-8 dark:text-gray-200">
-                {technologies.map((technology) => {
+                {props.project.technologies.map((technology) => {
                     return (
                         <li className=" bg-slate-500 text-white font-bold py-2 px-4 rounded-3xl opacity-70 cursor-not-allowed">
                             {technology}
@@ -29,15 +29,15 @@ const ProjectDetail = (props) => {
                 })}
             </ul>
              : <ul className=" justify-between flex px-16 my-8 dark:text-gray-200"><li className=" mx-auto bg-slate-500 text-white object-center font-bold py-2 px-4 rounded opacity-70 cursor-not-allowed">
-                {technologies[0]}
+                {props.project.technologies[0]}
             </li></ul>}
             <h3 className="text-3xl my-10 font-medium  pb-2 dark:text-gray-200">
                 {props.project.name}
             </h3>
             <p className="py-2 dark:text-gray-200">
-            {description}
+            {props.project.description}
             </p>
-            <p className=" dark:text-gray-200">Live app: <a href="" target={'_blank'}>{url}</a></p>
+            <p className=" dark:text-gray-200">Live app: <a href="" target={'_blank'}></a></p>
         </>
     )
 }
